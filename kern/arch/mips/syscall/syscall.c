3d5952968359a4e1ff6a226ae15d23b692699709
Pratyush Yadav <pratyush.mitma1@learner.manipal.edu>
2017-12-08 16:31:58 +0530

    Removed enter_forked_process(). We are using entrypoint() in proc_syscalls.c as the entrypoint for forked processes instead.

--- kern/arch/mips/syscall/syscall.c
+++ kern/arch/mips/syscall/syscall.c
@@ -197,17 +197,3 @@ syscall(struct trapframe *tf)
 	/* ...or leak any spinlocks */
 	KASSERT(curthread->t_iplhigh_count == 0);
 }
-
-/*
- * Enter user mode for a newly forked process.
- *
- * This function is provided as a reminder. You need to write
- * both it and the code that calls it.
- *
- * Thus, you can trash it and do things another way if you prefer.
- */
-void
-enter_forked_process(struct trapframe *tf)
-{
-	(void)tf;
-}
